7948d6ba25587203741ea6d6f0412b5aa5f6f6a0
Nick Baugh <niftylettuce@gmail.com>
2017-08-21 00:30:14 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+exports.plugins = [
+  [require('retext-sentence-spacing'), {preferred: 1}],
+  [require('retext-quotes'), {preferred: 'straight'}],
+  require('retext-diacritics'),
+  require('retext-redundant-acronyms'),
+  require('retext-repeated-words'),
+  require('retext-indefinite-article'),
+  require('retext-contractions')
+];
